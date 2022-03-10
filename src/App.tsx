5fcc603bf87d8a8444b7bed5f73f1fa66c32b17a
khaleel-lab <kbasha968@gmail.com>
2022-03-10 21:45:20 +0530

    Todo Logic & useRef Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 
-import { InputField } from './components';
 import './App.scss';
+
+import { InputField } from './components';
+import { TodoProps } from './constants/model';
 
 const App: React.FC = () => {
 	const [todo, setTodo] = useState<string>('');
-	const [todos, setTodos] = useState([]);
+	const [todos, setTodos] = useState<TodoProps[]>([]);
+
+	const handleAddTask = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+			setTodo('');
+		}
+	};
 
 	return (
 		<div className='app'>
 			<span className='app__heading'>Taskify</span>
 
-			<InputField todo={todo} setTodo={setTodo} />
+			<InputField todo={todo} setTodo={setTodo} handleAddTask={handleAddTask} />
+
+			{todos.map((t) => (
+				<li>{t.todo}</li>
+			))}
 		</div>
 	);
 };
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './InputField.scss';
 
 interface InputProps {
 	todo: string;
 	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	// e: React.FormEvent
+	// e: React.SyntheticEvent
+	// e: React.ChangeEvent<HTMLInputElement>
+	handleAddTask: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<InputProps> = ({ todo, setTodo }) => {
+const InputField: React.FC<InputProps> = ({ todo, setTodo, handleAddTask }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	return (
-		<form className='input'>
+		<form
+			className='input'
+			onSubmit={(e) => {
+				handleAddTask(e);
+				inputRef.current?.blur();
+			}}
+		>
 			<input
+				ref={inputRef}
 				type='input'
 				value={todo}
 				onChange={(e) => setTodo(e.target.value)}
